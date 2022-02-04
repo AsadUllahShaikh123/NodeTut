4f04ec210f9d50ba4b41fa140874fb61b50a6c13
AsadUllahShaikh123 <asadullahshaikh713@gmail.com>
2022-02-04 23:09:45 +0500

    connected node with mysql

--- codestepbystepnode/mongoosePractice/mongoosePostman/api.js
+++ codestepbystepnode/mongoosePractice/mongoosePostman/api.js
@@ -9,11 +9,57 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/create',async (req,resp)=>{
-  await staff.create(req.body);
-  resp.send({Datainserted:'Yes'});
-})
+// ----------------------------------------
 
-app.listen(5000);
+// Data inserted in Mongodb 
+
+// --------------------------------------
+
+// app.post('/create',async (req,resp)=>{
+//   await staff.create(req.body);
+//   resp.send({Datainserted:'Yes'});
+// })
+
+// ------------ Getting the Data without passing giving any schema is possible ------------
+
+// app.get('/get', async (req,resp)=>{
+//     let data = await staff.find();
+//     resp.send(data);
+// })
+
+// ------------- Deleting the Data ----------- 
+// app.delete('/delete/:name',async (req,resp)=>{
+  
+//   let result = await staff.deleteOne(req.params)
+//   resp.send({ deleted : result.deletedCount});
+
+// })
+
+
+// app.put('/update/:name', async (req,resp)=>{
+
+//    let result = await staff.updateOne(req.params,{$set:{name:'Azhar'}});
+//    resp.send({resultUpdated : result.acknowledged})
+// })
+
+
+// ---------- Searching with Single Field or Multiple Field incomplete -------------
+
+
+// app.get('/search/:name', async (req, resp)=>{
+//   let result = staff.find(
+//     {
+//       "$or":[
+//         {"name":{$regex : req.params.name}},
+
+//     ]
+//     }
+//   )
+// })
+
+
+// app.listen(5000);
+
+
 
 
